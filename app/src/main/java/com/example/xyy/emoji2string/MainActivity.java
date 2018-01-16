7bd52d3360ee8b21fa8baee34cf0b3da8cfac514
DaYang40 <2212750003@qq.com>
2018-01-16 19:09:56 +0800

    第二次提交

--- app/src/main/java/com/example/xyy/emoji2string/MainActivity.java
+++ app/src/main/java/com/example/xyy/emoji2string/MainActivity.java
@@ -42,6 +42,8 @@ public class MainActivity extends AppCompatActivity implements EmView, View.OnCl
     private EmPresenter emPresenter;
     private ProgressDialog pd;
 
+    private Button bt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
